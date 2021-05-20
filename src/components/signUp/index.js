08a2fa2cee7611544432d0933bfcfcd8f7c1f008
Hassan <hassan.alirobleh@gmail.com>
2021-05-20 12:14:04 +0200

    Les redirections

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,11 +1,130 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../Firebase";
+import { Link } from "react-router-dom";
 
-const Signup = () => {
-  return <div className="signUpLoginBox">
+const Signup = (props) => {
+  
+  const firebase = useContext(FirebaseContext);
+  // console.log(firebase);
+
+  const data = {
+    pseudo: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [loginData, setLoginData] = useState(data);
+  const [error, setError] = useState("");
+
+  // console.log(loginData);
+
+  const handleChange = (e) => {
+    setLoginData({ ...loginData, [e.target.id]: e.target.value });
+  };
+
+  // Méthode de submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = loginData;
+    firebase
+      .signupUser(email, password)
+      .then((user) => {
+        //console.log(user);
+        setLoginData({ ...data });
+        props.history.push("/welcome");
+      })
+      .catch((error) => {
+        setError(error);
+        setLoginData({ ...data });
+      });
+  };
+
+  // Destructuring
+  const { pseudo, email, password, confirmPassword } = loginData;
+
+  const btn =
+    pseudo === "" ||
+    email === "" ||
+    password === "" ||
+    password !== confirmPassword ? (
+      <button disabled>Inscription</button>
+    ) : (
+      <button>Inscription</button>
+    );
+
+  // Gestion des erreurs
+  const errorMsg = error !== "" && <span>{error.message}</span>;
+
+  return (
+    <div className="signUpLoginBox">
       <div className="slContainer">
-          signup
+        <div className="formBoxLeftSignup"></div>
+        <div className="formBoxRight">
+          <div className="formContent">
+            {errorMsg}
+            <h2>Inscription</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="inputBox">
+                <input
+                  onChange={handleChange}
+                  value={pseudo}
+                  type="text"
+                  id="pseudo"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="pseudo">Pseudo</label>
+              </div>
+
+              <div className="inputBox">
+                <input
+                  onChange={handleChange}
+                  value={email}
+                  type="email"
+                  id="email"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="inputBox">
+                <input
+                  onChange={handleChange}
+                  value={password}
+                  type="password"
+                  id="password"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="password">Mot de passe</label>
+              </div>
+
+              <div className="inputBox">
+                <input
+                  onChange={handleChange}
+                  value={confirmPassword}
+                  type="password"
+                  id="confirmPassword"
+                  autoComplete="off"
+                  required
+                />
+                <label htmlFor="confirmPassword">
+                  Confirmer le mot de passe
+                </label>
+              </div>
+              {btn}
+            </form>
+            <div className="linkContainer">
+              <Link className="simpleLink" to="/login">
+                Déjà inscrit? connectez-vous.
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Signup;
