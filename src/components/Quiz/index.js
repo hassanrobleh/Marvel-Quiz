2506147e365a691ee4b440b3962dbc505f6ca068
Hassan <hassan.alirobleh@gmail.com>
2021-05-24 20:04:14 +0200

    Récupérer les questions

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,20 +1,59 @@
 import React, { Component } from "react";
+import { QuizMarvel } from "../quizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
 
 class Quiz extends Component {
+  state = {
+    levelsNames: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0,
+  };
+
+  loadQuestions = (quizz) => {
+    const fetchArrayQuiz = QuizMarvel[0].quizz[quizz];
+
+    if (fetchArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({ storedQuestions: newArray });
+    } else {
+      console.log("Pas assez de question");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelsNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
+
   render() {
-    const { pseudo } = this.props.userData;
+    //const { pseudo } = this.props.userData;
     return (
       <div>
-        <h2>Pseudo: {pseudo}</h2>
+        {/* <h2>Pseudo: {pseudo}</h2> */}
         <Levels />
         <ProgressBar />
-        <h2>Notre question Quiz</h2>
-        <p className="answerOptions">Question 1</p>
-        <p className="answerOptions">Question 2</p>
-        <p className="answerOptions">Question 3</p>
-        <p className="answerOptions">Question 4</p>
+        <h2>{this.state.question}</h2>
+
+        {this.state.options.map((o, i) => (
+          <p key={i} className="answerOptions">
+            {o}
+          </p>
+        ))}
         <button className="btnSubmit">Suivant</button>
       </div>
     );
