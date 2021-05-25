370d1e12aa17af0bed28289fdfb254d9ec907ee3
Hassan <hassan.alirobleh@gmail.com>
2021-05-25 22:46:27 +0200

    validation de la réponse et gestion de score

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -12,12 +12,19 @@ class Quiz extends Component {
     question: null,
     options: [],
     idQuestion: 0,
+    btnDisable: true,
+    userAnswer: null,
+    score: 0,
   };
+
+  storedDataRef = React.createRef();
 
   loadQuestions = (quizz) => {
     const fetchArrayQuiz = QuizMarvel[0].quizz[quizz];
 
     if (fetchArrayQuiz.length >= this.state.maxQuestions) {
+      this.storedDataRef.current = fetchArrayQuiz;
+      //console.log(this.storedDataRef);
       const newArray = fetchArrayQuiz.map(
         ({ answer, ...keepRest }) => keepRest
       );
@@ -38,7 +45,44 @@ class Quiz extends Component {
         options: this.state.storedQuestions[this.state.idQuestion].options,
       });
     }
+
+    if (this.state.idQuestion !== prevState.idQuestion) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+        userAnswer: null,
+        btnDisable: true,
+      });
+    }
   }
+
+  submitAnswer = (selectedAnswer) => {
+    this.setState({
+      userAnswer: selectedAnswer,
+      btnDisable: false,
+    });
+  };
+
+  nextQuestion = () => {
+    if (this.state.idQuestion === this.state.maxQuestions - 1) {
+      // End
+    } else {
+      // Next
+      this.setState((prevState) => ({
+        idQuestion: prevState.idQuestion + 1,
+      }));
+    }
+
+    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer;
+    //console.log(goodAnswer);
+    //console.log(this.state.userAnswer);
+
+    if (this.state.userAnswer === goodAnswer) {
+      this.setState((prevState) => ({
+        score: prevState.score + 1,
+      }));
+    }
+  };
 
   render() {
     //const { pseudo } = this.props.userData;
@@ -49,12 +93,24 @@ class Quiz extends Component {
         <ProgressBar />
         <h2>{this.state.question}</h2>
 
-        {this.state.options.map((o, i) => (
-          <p key={i} className="answerOptions">
-            {o}
+        {this.state.options.map((option, index) => (
+          <p
+            onClick={() => this.submitAnswer(option)}
+            key={index}
+            className={`answerOptions ${
+              this.state.userAnswer === option ? "selected" : null
+            }`}
+          >
+            {option}
           </p>
         ))}
-        <button className="btnSubmit">Suivant</button>
+        <button
+          onClick={() => this.nextQuestion()}
+          disabled={this.state.btnDisable}
+          className="btnSubmit"
+        >
+          Suivant
+        </button>
       </div>
     );
   }
