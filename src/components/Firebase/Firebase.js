2085851efe9e68cc70b12cfb73e2573df5636b99
Hassan <hassan.alirobleh@gmail.com>
2021-05-28 13:31:04 +0200

    variable d'environnement pour firebase

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyCw9jzWKi__glgNrIUqv5GNM6ysu6C-SK8",
-  authDomain: "marvel-quiz-578d3.firebaseapp.com",
-  projectId: "marvel-quiz-578d3",
-  storageBucket: "marvel-quiz-578d3.appspot.com",
-  messagingSenderId: "648767091854",
-  appId: "1:648767091854:web:5894bc78564ffaebd185df",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 class Firebase {
